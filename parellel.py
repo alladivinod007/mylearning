2f9e792467e5312ac00345d243792fa557049b3b
alladivinod007 <121356258+alladivinod007@users.noreply.github.com>
2023-07-26 20:50:40 +0530

    Update parellel.py

--- parellel.py
+++ parellel.py
@@ -1,63 +1,99 @@
-from flask import Flask, jsonify
-from pymongo import MongoClient
-import concurrent.futures
+import pymongo
+import pymysql
+from pymongo import UpdateOne
+from concurrent.futures import ThreadPoolExecutor
 
-app = Flask(__name__)
+# Function to fetch column information from SQL Server table
+def fetch_sql_column_info(sql_server_params, query):
+    conn = pymysql.connect(**sql_server_params)
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
 
-# MongoDB connection
-client = MongoClient('mongodb://localhost:27017')
-db = client['your_database']
+    cursor.execute(query)
+    column_info = cursor.fetchall()
 
-# Define the aggregation pipelines for each collection
-pipelines = {
-    'collection1': [
-        {"$match": {"field": "value1"}},
-        {"$limit": 10}
-    ],
-    'collection2': [
-        {"$match": {"field": "value2"}},
-        {"$limit": 10}
-    ],
-    'collection3': [
-        {"$match": {"field": "value3"}},
-        {"$limit": 10}
-    ],
-    'collection4': [
-        {"$match": {"field": "value4"}},
-        {"$limit": 10}
-    ],
-    'collection5': [
-        {"$match": {"field": "value5"}},
-        {"$limit": 10}
-    ]
-}
+    cursor.close()
+    conn.close()
 
-# Define a function to execute the pipeline for a collection
-def execute_pipeline(collection_name, pipeline):
-    result = list(db[collection_name].aggregate(pipeline))
-    return result
+    return column_info
 
-# Define a route to perform parallel matching
-@app.route('/match_parallel')
-def match_parallel():
-    matched_records = []
-    
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+# Function to fetch data from SQL Server
+def fetch_data_from_sql(sql_server_params, query, start_id, chunk_size):
+    conn = pymysql.connect(**sql_server_params)
+    cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+    query_with_limits = f"{query} WHERE accnt_nmbr >= {start_id} LIMIT {chunk_size}"
+    cursor.execute(query_with_limits)
+    data = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+
+    return data
+
+# Function to process a chunk of data
+def process_chunk(chunk_data, column_info, mongo_params):
+    client = pymongo.MongoClient(**mongo_params)
+    db = client['your_database']
+    collection = db['loadData']
+
+    bulk_operations = []
+    for item in chunk_data:
+        accnt_nmbr = item['accnt_nmbr']
+        product_info = {'accnt_nmbr': accnt_nmbr}
+        for column in column_info:
+            if column['Field'] != 'accnt_nmbr':
+                product_info[column['Field']] = item[column['Field']]
+
+        filter_query = {'accnt_nmbr': accnt_nmbr}
+        update_query = {'$set': {'product': product_info}}
+        update_one = UpdateOne(filter_query, update_query, upsert=True)
+        bulk_operations.append(update_one)
+
+    if bulk_operations:
+        collection.bulk_write(bulk_operations)
+
+    client.close()
+
+# Function to load data in chunks using ThreadPoolExecutor
+def load_data_with_threads(sql_server_params, query, chunk_size, mongo_params, max_threads=5):
+    column_info = fetch_sql_column_info(sql_server_params, query)
+    total_records = fetch_total_records(sql_server_params, query)
+
+    with ThreadPoolExecutor(max_workers=max_threads) as executor:
         futures = []
-        
-        for collection_name, pipeline in pipelines.items():
-            future = executor.submit(execute_pipeline, collection_name, pipeline)
+        for i in range(0, total_records, chunk_size):
+            data = fetch_data_from_sql(sql_server_params, query, i, chunk_size)
+            future = executor.submit(process_chunk, data, column_info, mongo_params)
             futures.append(future)
-        
-        for future in concurrent.futures.as_completed(futures):
-            matching_records = future.result()
-            matched_records.extend(matching_records)
-            
-            if len(matched_records) >= 10:
-                break
-    
-    return jsonify(matched_records[:10])  # Limit to 10 documents
 
-# Run the Flask app
+        for future in futures:
+            future.result()
+
+# Function to fetch total records from SQL Server
+def fetch_total_records(sql_server_params, query):
+    conn = pymysql.connect(**sql_server_params)
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT COUNT(*) FROM ({query}) AS total")
+    total_records = cursor.fetchone()[0]
+
+    cursor.close()
+    conn.close()
+
+    return total_records
+
 if __name__ == '__main__':
-    app.run()
+    sql_server_params = {
+        'host': 'your_sql_server_host',
+        'user': 'your_username',
+        'password': 'your_password',
+        'database': 'your_database'
+    }
+    query = 'SELECT * FROM your_table_name'
+    chunk_size = 1000  # Adjust the chunk size as needed
+    mongo_params = {
+        'host': 'your_mongodb_uri'
+    }
+    max_threads = 5  # Adjust the maximum number of threads as needed
+
+    load_data_with_threads(sql_server_params, query, chunk_size, mongo_params, max_threads)
